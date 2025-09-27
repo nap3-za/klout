981608b82e9fb2a7fdda6c96a978dbed64151a03
Nape <nap3ntsoane@protonmail.com>
2025-09-27 21:21:00 +0200

    abi files added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+import('hardhat/config.js').HardhatUserConfig
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
