51639f801cb980bbb9495da5b5abcccdcd8a349f
Nape <nap3ntsoane@protonmail.com>
2025-09-27 20:40:53 +0200

    address access corrected

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,20 +4,23 @@ async function main() {
   // deploy my token
   const Token = await hre.ethers.getContractFactory("NAPE");
   const token = await Token.deploy(1_000_000); // 1 million tokens, MILLIONAIRRREEE, RICH
-  await token.deployed();
-  console.log("NFX Token deployed to:", token.address);
+  await token.waitForDeployment();
+  const tokenAddress = await token.getAddress();
+  console.log("NFX Token deployed to:", tokenAddress);
 
   // userprofiles
   const Profiles = await hre.ethers.getContractFactory("UserProfiles");
   const profiles = await Profiles.deploy();
-  await profiles.deployed();
-  console.log("UserProfiles deployed to:", profiles.address);
+  await profiles.waitForDeployment();
+  const profilesAddress = await profiles.getAddress();
+  console.log("UserProfiles deployed to:", profilesAddress);
 
   // wagermanager with NFX token + userprofiles
   const WagerManager = await hre.ethers.getContractFactory("WagerManager");
-  const wagers = await WagerManager.deploy(token.address, profiles.address);
-  await wagers.deployed();
-  console.log("WagerManager deployed to:", wagers.address);
+  const wagers = await WagerManager.deploy(tokenAddress, profilesAddress);
+  await wagers.waitForDeployment();
+  const wagersAddress = await wagers.getAddress();
+  console.log("WagerManager deployed to:", wagersAddress);
 }
 
 main().catch((error) => {
