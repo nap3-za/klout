d89fc44b277a83c1c8eec034aded15e3733e65bf
Nape <nap3ntsoane@protonmail.com>
2025-09-27 20:35:41 +0200

    mended imports

--- contracts/WagerManager.sol
+++ contracts/WagerManager.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "openzeppelin-contracts/utils/Address.sol";
-import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IUserProfiles {
     function updateStats(
